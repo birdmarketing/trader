ede785394d16eb99809f9899221ea4668e10691a
birdmarketing <web@birdmarketing.co.uk>
2018-11-02 11:59:36 +0000

    File Reader

--- Cron/Import.php
+++ Cron/Import.php
@@ -5,13 +5,40 @@ namespace BirdMarketing\Trader\Cron;
 use Zend\Log\Writer\Stream;
 use Zend\Log\Logger;
 
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\File\Csv;
+use Magento\Framework\Filesystem;
+use Magento\Framework\App\Filesystem\DirectoryList;
+
 class Import {
 
+	private $objectManager;
+	private $filesystem;
+	private $csv;
+
+	private $writer;
+	private $logger;
+
+	public function __constructor(ObjectManager $objectManager, Filesystem $filesystem, Csv $csv) {
+		$this->objectManager = $objectManager;
+		$this->filesystem = $filesystem;
+		$this->csv = $csv;
+
+		// setup logger with file writer
+		$this->writer = new Stream(BP . '/var/log/trader.log');
+		$this->logger = new Logger();
+		$this->logger->addWriter($this->writer);
+	}
+
 	public function execute(): void {
-		$writer = new Stream(BP . '/var/log/cron.log');
-		$logger = new Logger();
-		$logger->addWriter($writer);
-		$logger->info('Import -> execute()');
+		$this->logger->info('Import started');
+
+		$tmp = $this->filesystem->getDirectoryRead(DirectoryList::ROOT);
+		$this->logger->info(print_r($tmp, true));
+	}
+
+	private function import(): void {
+
 	}
 
 }
